33f00c109504f062632d0dd708110166836c4f79
Brian Kereszturi <briankereszturi@gmail.com>
2017-07-17 13:18:36 -0700

    Copy static/ to dist/

--- iso-site.js
+++ iso-site.js
@@ -4,6 +4,8 @@
 const spawnSync = require('child_process').spawnSync;
 const path = require('path');
 const webpack = require('webpack');
+const fs = require('fs');
+const ncp = require('ncp').ncp;
 
 const command = process.argv[2];
 
@@ -11,6 +13,22 @@ const dir = './node_modules/iso-site';
 const nodePath = `${process.env.NODE_PATH || ''}:./src:./node_modules:${dir}/node_modules`;
 
 const pkg = require(path.join(process.cwd(), 'package.json'));
+
+// Always copy static/ to dist/ if it exists.
+const staticDir = path.join(process.cwd(), 'static');
+const distDir = path.join(process.cwd(), 'dist');
+if (fs.existsSync(staticDir)) {
+  if (!fs.existsSync(distDir)) {
+    fs.mkdirSync(distDir);
+  }
+
+  ncp(staticDir, distDir, (error) => {
+    if (error) {
+      console.log('Error copy static files:', error.message);
+      process.exit(1);
+    }
+  });
+}
 
 switch (command) {
   case "build": {
